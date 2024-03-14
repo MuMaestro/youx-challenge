c5897c9a33d54e0ba8ae460c0989da56372fb707
Murilo Augusto Maestro <murilomaestro@gmail.com>
2024-03-14 20:47:39 -0300

    refactor(web): trocando paths  de rewrite para api e arquivos do projeto

--- apps/youx-front/next.config.js
+++ apps/youx-front/next.config.js
@@ -7,16 +7,25 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
+	async rewrites() {
+		return [
+			{
+				source: '/api/auth/:path*',
+				destination: 'http://localhost:3001/api/auth/:path*',
+			},
+			{
+				source: '/api/profile/:path*',
+				destination: 'http://localhost:3001/api/profile/:path*',
+			}
+		]
+	},
+	nx: {
+		svgr: true,
+	},
 };
 
 const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
+	withNx,
 ];
 
 module.exports = composePlugins(...plugins)(nextConfig);
