a1bc6607e446b36419d8454c83d63f7381cf531a
Murilo Augusto Maestro <murilomaestro@gmail.com>
2024-03-18 12:31:56 -0300

    feat(backend): adicionando lista de vendas

--- apps/youx-backend/src/main/java/server/models/ClienteModel.java
+++ apps/youx-backend/src/main/java/server/models/ClienteModel.java
@@ -1,6 +1,7 @@
 package server.models;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -8,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -22,10 +24,12 @@ public class ClienteModel implements Serializable {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
 	@Column(unique = true)
-	private String cpnj;
+	private String cnpj;
 	private String name;
 	private String email;
 	private String telefone;
 	private String uf;
+	@OneToMany
+	private List<VendaModel> vendas;
 
 }
